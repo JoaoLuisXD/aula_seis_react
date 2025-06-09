3e8474b4cfe9d169ef5c386fbde987b838147f51
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-06-09 09:55:02 -0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+import {Button} from 'primereact/button';
+
+
+const App = () => {
+  return (
+    <>
+    <Button label="Submit"/>
+    </>
+  );
+}
+ 
+export default App;
