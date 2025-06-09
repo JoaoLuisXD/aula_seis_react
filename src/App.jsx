f513c41b322f35d2fae5e14ad03b960ef2b7fb44
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-06-09 10:52:32 -0300

    update login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
-
-import {Button} from 'primereact/button';
+import Paths from "./routes/Paths";
 
 
 const App = () => {
   return (
     <>
-    <Button label="Submit"/>
+      <Paths/>
     </>
   );
 }
