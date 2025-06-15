648c6ec878348988ee05bc788f6f9fd9fe79371b
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-06-15 12:58:43 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import "primereact/resources/themes/lara-dark-cyan/theme.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 import Paths from "./routes/Paths";
+import { AuthContext } from "./contexts/AuthContext";
 
 
 const App = () => {
+
   return (
     <>
-      <Paths/>
+      <AuthContext>
+        <Paths/>
+      </AuthContext>
+      
     </>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,24 +2,43 @@ import { InputText } from 'primereact/inputtext';
 import { IconField } from 'primereact/iconfield';
 import { InputIcon } from 'primereact/inputicon';
 import {Button} from 'primereact/button';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Context } from '../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
         
         
 
 const Login = () => {
     const[mostraSenha,setMostrarSenha] = useState(false);
+    
+    const{register, handleSubmit} = useForm();
+
+    const {setLogado} = useContext(Context)
+    
+    const navigate = useNavigate();
+
+    function logar(dados){
+        if(dados.email == "joaozinho@gmail.com" && dados.senha == "joaozinho123")
+        {
+            setLogado(true);
+            navigate('/home');
+        }
+    }
+
     return (
         <>
             <div style={{ backgroundColor: 'var(--cyan-900)' }}  className=' min-h-screen flex align-items-center justify-content-center px-3'>
-                <form className='col-12 md:col-3 p-3 border-round-md'  style={{ backgroundColor: 'var(--gray-900)' }}>
+                <form onSubmit={handleSubmit(logar)} className='col-12 md:col-3 p-3 border-round-md'  style={{ backgroundColor: 'var(--gray-900)' }}>
 
-                    <h3 style={{color: 'var(--primay-50)'}}>Seja Bem-Vindo</h3>
+                    <h2 style={{color: 'var(--primay-50)'}} className='text-center'>Seja Bem-Vindo</h2>
                     <label htmlFor="email" className='block uppercase font-bold text -sm mb-1' style={{color: 'var(--primay-50)'}}>Email</label>
                     <InputText
                         id='email'
                         type='email'
                         placeholder='email@email.com'
                         className='mb-3 w-full'
+                        {...register('email', {required:true} )}
                     />
                     <label htmlFor="senha " className='block uppercase font-bold text -sm mb-1' style={{color: 'var(--primay-50)'}}>Senha</label>
                     <div className='mb-3'>
@@ -33,6 +52,7 @@ const Login = () => {
                                 type={mostraSenha ? 'text' : 'password'}
                                 placeholder='********'
                                 className='w-full'
+                                {...register('senha', {required:true} )}
                             />
                         </IconField>
                     </div>
