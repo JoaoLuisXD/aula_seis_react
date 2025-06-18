9d510f35b89cc051ca5e56ba9fec14a3a2bc57d8
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-06-18 16:35:17 -0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,39 +25,21 @@ const Home = () => {
                 <h1>Lista de Produtos</h1>
 
                 <ul className="grid list-none p-0">
-                    <li className='col-12 md:col-3'>
+                    {
+                        products.map((product) => (
+                        <li className='col-12 md:col-3'>
                         <div className="shadow-4 border-round-md">
                             <div className="relative">
-                                <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" className="w-full"/>
-                                <h6 className="absolute top-0 right-0 py-1 px-2 border-round-md" style={{backgroundColor: 'var(--blue-800)', marginRight:'4px'}}>4.9</h6>                   
+                                <img src={product.image} alt={product.tittle} className="w-full" style={{height: '300px', objectFit: 'contain'}}/>
+                                <h6 className="absolute top-0 right-0 py-1 px-2 border-round-md" style={{backgroundColor: 'var(--blue-800)', marginRight:'4px'}}>{product.rating.rate}</h6>                   
                             </div>
-                            <h3 className="mb-0">Bolsa</h3>
-                            <h6 className="mt-1 uppercase" style={{color: 'var(--blue-600)'}}>Categoria</h6>
-                            <h2>R$ 79,99</h2>
-
-                            
+                            <h3 className="mb-0">{product.title}</h3>
+                            <h6 className="mt-1 uppercase" style={{color: 'var(--blue-600)'}}>{product.category}</h6>
+                            <h2>$ {product.price}</h2>
                         </div>
                     </li>
-                    <li className='col-12 md:col-3'>
-                        <div className="shadow-4 border-round-md" style={{backgroundColor: 'var(--teal-900)'}}>
-                            produto2
-                        </div>
-                    </li>
-                    <li className='col-12 md:col-3'>
-                        <div className="shadow-4 border-round-md" style={{backgroundColor: 'var(--teal-900)'}}>
-                            produto3
-                        </div>
-                    </li>
-                    <li className='col-12 md:col-3'>
-                        <div className="shadow-4 border-round-md" style={{backgroundColor: 'var(--teal-900)'}}>
-                            produto4
-                        </div>
-                    </li>
-                    <li className='col-12 md:col-3'>
-                        <div className="shadow-4 border-round-md" style={{backgroundColor: 'var(--teal-900)'}}>
-                            produto5
-                        </div>
-                    </li>
+                    ))
+                    }
                 </ul>
             </section>
         </>
